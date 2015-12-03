95ebf4f2ce5654aef9abf3db5f14fe3c278d3434
Dejan Ristic <dejan.ristic@factoryww.com>
2015-12-03 12:14:46 +0100

    finished view function

--- static/js/view.js
+++ static/js/view.js
@@ -24,40 +24,52 @@ $(document).ready(function() {
 
     displayCards = function(response){
 
-        var $gamesContainer = $(".games-container");
-        var allResults = "";
-        var dealer = response["current_game"]["dealer"];
-        var players = response["current_game"]["players"];
+        function gameResults(game){
+            var allResults = "";
+            var dealer = game["dealer"];
+            var players = game["players"];
 
-        var gameDealer = "";
-        gameDealer += ('<div class="res-container"><div class="results"><div class="name">'+dealer["name"]+'</div>');
-        for(i= 0; i<dealer["hand"]["cards"].length; i++) {
-            gameDealer += ('<div class="cards">'+dealer["hand"]["cards"][i]["color"]+'</div>');
-            gameDealer += ('<div class="cards">'+dealer["hand"]["cards"][i]["number"]+'</div>');
-            gameDealer += (i == dealer["hand"]["cards"].length-1) ? "</div>" : "";
-        }
-        allResults += gameDealer;
-
-        var gamePlayer = "";
-        for(i= 0; i<players.length; i++) {
-            gamePlayer += ('<div class="results"><div class="name">'+players[i]["name"]+'</div>');
-            for(j=0; j<players[i]["hand"]["cards"].length; j++){
-                gamePlayer += ('<div class="cards">'+players[i]["hand"]["cards"][j]["color"]+'</div>');
-                gamePlayer += ('<div class="cards">'+players[i]["hand"]["cards"][j]["number"]+'</div>');
-                gamePlayer += (j == players[i]["hand"]["cards"].length-1) ? "</div>" : "";
+            var gameDealer = "";
+            gameDealer += ('<div class="res-container"><div class="results"><div class="name">'+dealer["name"]+'</div>');
+            for(i= 0; i<dealer["hand"]["cards"].length; i++) {
+                gameDealer += ('<div class="cards">'+dealer["hand"]["cards"][i]["color"]+'</div>');
+                gameDealer += ('<div class="cards">'+dealer["hand"]["cards"][i]["number"]+'</div>');
+                gameDealer += (i == dealer["hand"]["cards"].length-1) ? "</div>" : "";
             }
-            gamePlayer += (i == players.length-1) ? "</div>" : "";
+            allResults += gameDealer;
+
+            var gamePlayer = "";
+            for(i= 0; i<players.length; i++) {
+                gamePlayer += ('<div class="results"><div class="name">'+players[i]["name"]+'</div>');
+                for(j=0; j<players[i]["hand"]["cards"].length; j++){
+                    gamePlayer += ('<div class="cards"><span>|</span>'+players[i]["hand"]["cards"][j]["color"]+'-</div>');
+                    gamePlayer += ('<div class="cards">'+players[i]["hand"]["cards"][j]["number"]+'<span>|</span></div>');
+                    gamePlayer += (j == players[i]["hand"]["cards"].length-1) ? "</div>" : "";
+                }
+                gamePlayer += (i == players.length-1) ? "</div>" : "";
+            }
+            allResults += gamePlayer;
+            return allResults;
         }
-        allResults += gamePlayer;
 
-        if (response["games"].length != bot.getGameNumber()){
-            $gamesContainer.find(".res-container").last().remove();
+        var toDisplay = "";
+        var $gamesContainer = $(".games-container");
+
+        $gamesContainer.find(".res-container").last().remove();
+
+        if (!(response["games"].length < bot.getGameNumber()) && response["games"].length){
+            toDisplay = gameResults(response["games"][response["games"].length-1]);
+            $gamesContainer.append(toDisplay);
         }
-        $gamesContainer.append(allResults);
+
+        toDisplay = gameResults(response["current_game"]);
+        $gamesContainer.append(toDisplay);
 
 
 
 
+        var dealerCurrent = response["current_game"]["dealer"];
+        var playersCurrent = response["current_game"]["players"];
 
         var $scores = $(".scores");
         var $scoresPlayers = $scores.find(".games-players");
@@ -66,12 +78,12 @@ $(document).ready(function() {
         $scores.find(".games-count").find(".total-played").text(response["games"].length);
 
         var houseWins = "";
-        houseWins += '<div class="games-count"><div>'+dealer["name"]+'</div><div>'+response["house_wins"]+'</div></div>';
+        houseWins += '<div class="games-count"><div>'+dealerCurrent["name"]+'</div><div>'+response["house_wins"]+'</div></div>';
         $scoresPlayers.append(houseWins);
 
-        for(i= 0; i<players.length; i++) {
+        for(i= 0; i<playersCurrent.length; i++) {
             var playerWins = "";
-            playerWins += '<div class="games-count"><div>'+players[i]["name"]+'</div><div>'+players[i]["wins"]+'</div></div>';
+            playerWins += '<div class="games-count"><div>'+playersCurrent[i]["name"]+'</div><div>'+playersCurrent[i]["wins"]+'</div></div>';
             $scoresPlayers.append(playerWins);
         }
 
